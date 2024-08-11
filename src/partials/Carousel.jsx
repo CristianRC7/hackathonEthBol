e2166e43e7611895e8f4a4274a928865f60de795
Cristian Ramirez <cristian25ramirezrc@gmail.com>
2024-08-11 02:19:53 -0400

    cambios en el responsivo carousel

--- src/partials/Carousel.jsx
+++ src/partials/Carousel.jsx
@@ -5,6 +5,7 @@ import imagen2 from '../images/rendimiento.png';
 import imagen3 from '../images/rapidez.png';
 import imagen4 from '../images/eficacia.png';
 import imagen5 from '../images/frase.png';
+import '../styles/carousel.css';
 
 const images = [imagen1, imagen2, imagen3, imagen4, imagen5]; 
 
@@ -46,8 +47,8 @@ function Carousel() {
         <div className="pb-12 md:pb-20">
 
           {/* Carousel container */}
-          <div className="relative bg-gray-900 rounded-lg shadow-lg overflow-hidden">
-            <div className="relative w-full h-96 md:h-[32rem] overflow-hidden">
+          <div className="carousel-container">
+            <div className="carousel-images">
               {/* Carousel images */}
               <div className="absolute inset-0">
                 {images.map((image, index) => (
@@ -55,24 +56,26 @@ function Carousel() {
                     key={index}
                     src={image}
                     alt={`Imagen ${index + 1}`}
-                    className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
+                    className={`carousel-image ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
                   />
                 ))}
               </div>
 
               {/* Carousel controls */}
-              <button
-                onClick={handlePrev}
-                className="absolute top-1/2 left-4 transform -translate-y-1/2 p-3 bg-gray-800 text-white rounded-full focus:outline-none shadow-lg hover:bg-gray-700"
-              >
-                <FaChevronLeft size={24} />
-              </button>
-              <button
-                onClick={handleNext}
-                className="absolute top-1/2 right-4 transform -translate-y-1/2 p-3 bg-gray-800 text-white rounded-full focus:outline-none shadow-lg hover:bg-gray-700"
-              >
-                <FaChevronRight size={24} />
-              </button>
+              <div className="carousel-controls">
+                <button
+                  onClick={handlePrev}
+                  className="carousel-button"
+                >
+                  <FaChevronLeft size={24} />
+                </button>
+                <button
+                  onClick={handleNext}
+                  className="carousel-button"
+                >
+                  <FaChevronRight size={24} />
+                </button>
+              </div>
             </div>
           </div>
 
